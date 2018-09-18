d832e6816dc5bc1b338865b706df718dc0fe9e64
Michael Mason <mmason@opengenius.com>
2018-09-18 13:45:04 +0100

    Initial commit

---
+++ rn-cli.config.js
@@ -0,0 +1,10 @@
+const Metro = require('metro');
+
+Metro.Logger.on('log', logEntry => {
+    if (
+      logEntry.action_name === 'Transforming file' &&
+      logEntry.action_phase === 'end'
+    ) {
+      console.log(logEntry.file_name, logEntry.duration_ms);
+    }
+  });
